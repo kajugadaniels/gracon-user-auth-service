8996329bc67d8e9c0e1438d2c976c13b841b5098
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-03-20 21:34:20 +0200

    feat: add UsersModule wiring UsersService, UsersController, and CitizenModule

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,4 +1,14 @@
 import { Module } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { UsersController } from './users.controller';
+import { CitizenModule } from '../citizen/citizen.module';
 
-@Module({})
+@Module({
+  imports: [
+    CitizenModule, // provides CitizenService for NID lookup during registration
+  ],
+  controllers: [UsersController],
+  providers: [UsersService],
+  exports: [UsersService], // exported for AuthModule to use findByEmail and findById
+})
 export class UsersModule {}
