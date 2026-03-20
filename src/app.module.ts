c4bbc10186e1835d0381eb3f956b73bcaf55cfde
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-03-20 22:40:26 +0200

    feat: register S3Module in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { UsersModule } from './modules/users/users.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { CitizenModule } from './modules/citizen/citizen.module';
 import { AppMailerModule } from './common/mailer/mailer.module';
+import { S3Module } from './common/aws/s3/s3.module';
 
 @Module({
   imports: [
@@ -21,6 +22,7 @@ import { AppMailerModule } from './common/mailer/mailer.module';
     EncryptionModule, // AES-256 encryption + SHA-256 hashing
     PidModule, // platform ID generation
     AppMailerModule, // email sending service
+    S3Module, // AWS S3 file handling
 
     // Feature modules
     UsersModule,
