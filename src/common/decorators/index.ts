44c5a6cddb99b51b273ae3d59e7272fceb04d14c
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-03-21 21:26:35 +0200

    chore(decorators): export throttle decorators from barrel

---
+++ src/common/decorators/index.ts
@@ -0,0 +1,8 @@
+export { CurrentUser } from './current-user.decorator';
+export { RequireTokenType, TOKEN_TYPE_KEY } from './token-type.decorator';
+export {
+  ThrottleAuth,
+  ThrottleStrict,
+  ThrottleGeneral,
+  SkipThrottle,
+} from './throttle.decorator';
