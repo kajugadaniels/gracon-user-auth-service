c6a1693af87ec90e72a6c003b73d45ea72305066
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-03-21 20:06:19 +0200

    feat: register PasswordResetService and PasswordResetController in AuthModule

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -4,6 +4,8 @@ import { PassportModule } from '@nestjs/passport';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { PasswordResetService } from './password-reset.service';
+import { PasswordResetController } from './password-reset.controller';
 import { JwtStrategy } from './strategies/jwt.strategy';
 import { JwtAuthGuard } from '../../common/guards/jwt-auth.guard';
 import { UsersModule } from '../users/users.module';
@@ -27,8 +29,8 @@ import { UsersModule } from '../users/users.module';
 
     UsersModule,
   ],
-  controllers: [AuthController],
-  providers: [AuthService, JwtStrategy, JwtAuthGuard],
+  controllers: [AuthController, PasswordResetController],
+  providers: [AuthService, PasswordResetService, JwtStrategy, JwtAuthGuard],
   exports: [AuthService, JwtStrategy, PassportModule, JwtAuthGuard],
 })
 export class AuthModule {}
