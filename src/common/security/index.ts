834fb644003d15b35b841a6039be421d8080a6fb
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-03-22 03:36:30 +0200

    chore(auth): export SecurityEventService and SecurityEventModule from security barrel

--- src/common/security/index.ts
+++ src/common/security/index.ts
@@ -1,3 +1,5 @@
-export { buildHelmetConfig }    from './helmet.config';
-export { buildCorsConfig }      from './cors.config';
-export { DocsAuthMiddleware }   from './docs-auth.middleware';
+export { buildHelmetConfig } from './helmet.config';
+export { buildCorsConfig } from './cors.config';
+export { DocsAuthMiddleware } from './docs-auth.middleware';
+export { SecurityEventService } from './security-event.service';
+export { SecurityEventModule } from './security-event.module';
